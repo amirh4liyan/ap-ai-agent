60f7b6dbdf84135eb980c5f257b067fb73fcec37
Amirhossein Alian <alianmail81@gmail.com>
2025-02-04 17:30:36 +0330

    additional csv file added

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,9 @@ repositories {
 }
 
 dependencies {
+    implementation("org.neo4j.driver:neo4j-java-driver:4.4.2")
+    implementation("com.opencsv:opencsv:5.5.2")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
