3682c17aeeb3b3638bccdbebc227bac4e6edf578
Amirhossein Alian <alianmail81@gmail.com>
2025-02-05 20:55:24 +0330

    json proccess added to Abstract class

--- src/main/java/edu/sharif/AbstractLLMClient.java
+++ src/main/java/edu/sharif/AbstractLLMClient.java
@@ -2,24 +2,41 @@ package edu.sharif;
 
 import java.net.*;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public abstract class AbstractLLMClient {
 
+    protected final ObjectMapper objectMapper;
+    protected String query = "";
     protected String APIKEY = "";
     protected String APIURL = "";
     protected HttpURLConnection connection;
     protected URL url;
 
     public AbstractLLMClient(String APIKEY, String APIURL) {
+        this.objectMapper = new ObjectMapper();
         this.APIKEY = APIKEY;
         this.APIURL = APIURL;
     }
 
-    public abstract String generateCypherQuery(String userInput);
+    public abstract void generateCypherQuery(String userInput);
 
-    public void main(String[] args) {
-        String userInput = "What is the relationship between Ali and Maryam?";
+    public JsonNode parseJson(String jsonString) throws Exception {
+        return objectMapper.readTree(jsonString);
+    }
+    public String escapeJson(String input) {
+        return input.replace("\"", "\\\"").replace("\n", "\\n").replace("\r", "\\r");
+    }
 
-        String cypherQuery = generateCypherQuery(userInput);
-        System.out.println("Generated Cypher Query: " + cypherQuery);
+    public String getQuery() {
+        return query;
+    }
+
+    private String extractValue(JsonNode jsonNode, String fieldName) {
+        if (jsonNode.has(fieldName)) {
+            return jsonNode.get(fieldName).asText().trim().replace("`", "");
+        }
+        return null;
     }
 }
