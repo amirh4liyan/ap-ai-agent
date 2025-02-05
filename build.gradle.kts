91fcde44d2bdf4764566da9d6bb00127f1e02aa5
Amirhossein Alian <alianmail81@gmail.com>
2025-02-05 20:51:10 +0330

    dependency for json

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     implementation("org.neo4j.driver:neo4j-java-driver:4.4.2")
     implementation("com.opencsv:opencsv:5.5.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.0")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.0")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
