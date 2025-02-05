5a633f73286539a290bc49ad9801b4a8bc906e75
Amirhossein Alian <alianmail81@gmail.com>
2025-02-05 13:11:06 +0330

    abstract class it is a template for making Connection between app and LLMs

--- src/main/java/edu/sharif/AbstractLLMClient.java
+++ src/main/java/edu/sharif/AbstractLLMClient.java
@@ -1,5 +1,25 @@
 package edu.sharif;
 
+import java.net.*;
+
 public abstract class AbstractLLMClient {
-    
+
+    protected String APIKEY = "";
+    protected String APIURL = "";
+    protected HttpURLConnection connection;
+    protected URL url;
+
+    public AbstractLLMClient(String APIKEY, String APIURL) {
+        this.APIKEY = APIKEY;
+        this.APIURL = APIURL;
+    }
+
+    public abstract String generateCypherQuery(String userInput);
+
+    public void main(String[] args) {
+        String userInput = "What is the relationship between Ali and Maryam?";
+
+        String cypherQuery = generateCypherQuery(userInput);
+        System.out.println("Generated Cypher Query: " + cypherQuery);
+    }
 }
