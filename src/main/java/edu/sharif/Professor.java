8e3b76569c08973b4a91615e81eebf3e584e0094
Amirhossein Alian <alianmail81@gmail.com>
2025-02-06 17:43:17 +0330

    make unused function private

--- src/main/java/edu/sharif/Professor.java
+++ src/main/java/edu/sharif/Professor.java
@@ -94,45 +94,24 @@ public class Professor implements GraphEntity {
         return map;
     }
 
-
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getDepartment() {
         return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
     }
 
     public String getTeaching() {
@@ -147,10 +126,6 @@ public class Professor implements GraphEntity {
         return collaboration_start_year;
     }
 
-    public void setCollaboration_start_year(String collaboration_start_year) {
-        this.collaboration_start_year = collaboration_start_year;
-    }
-
     public String getResearch_interests() {
         return research_interests;
     }
@@ -161,10 +136,6 @@ public class Professor implements GraphEntity {
 
     public String getPublications_file() {
         return publications_file;
-    }
-
-    public void setPublications_file(String publications_file) {
-        this.publications_file = publications_file;
     }
 
 }
